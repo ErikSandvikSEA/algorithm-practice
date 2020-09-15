6648d6c3a66d55ede140b1dbd724e6fdb95b067a
eriksandviksea <eriksandvikSEA@Gmail.com>
2020-09-15 14:13:06 -0700

    trimBST

---
+++ trimBST.js
@@ -0,0 +1,14 @@
+function trimBST(root, low, high) {
+    if(!root){
+        return null
+    }
+    if(root.val > high){
+        return trimBST(root.left, low, high)
+    }
+    if(root.val < low){
+        return trimBST(root.right, low, high)
+    }
+    root.left = trimBST(root.left, low, high)
+    root.right = trimBST(root.right, low, high)
+    return root
+};
