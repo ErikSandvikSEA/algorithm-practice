227c8326af0b5b27d6d15c0e4b91f1a9e08f8167
eriksandviksea <eriksandvikSEA@Gmail.com>
2020-09-25 10:46:07 -0700

    oddEvenList

---
+++ oddEvenList.js
@@ -0,0 +1,17 @@
+function oddEvenList(head) {
+    if (!head) return head;
+  
+    let odd = head;
+    let even = head.next;
+    while (odd.next && odd.next.next) {
+      let tmp = odd.next;
+      odd.next = odd.next.next;
+      odd = odd.next;
+      tmp.next = odd.next;
+    }
+    odd.next = even;
+    return head;
+  }
+
+
+  
