6d625b528eeec7899bf7736cb3764868ee738094
eriksandviksea <eriksandvikSEA@Gmail.com>
2020-11-03 10:39:15 -0800

    numEquivDominoPiars

--- numEquivDominoPairs.js
+++ numEquivDominoPairs.js
@@ -1,8 +1,9 @@
 var numEquivDominoPairs = function(dominoes) {
-    let arr = Array(40000).fill(0), pairCount = 0;
+    let arr = Array(100).fill(0)
+    let pairCount = 0;
     
-    for(const d of dominoes){
-        const num = d[0] < d[1] ? d[0] * 10 + d[1] : d[1] * 10 + d[0];
+    for(const domino of dominoes){
+        const num = domino[0] < domino[1] ? domino[0] * 10 + domino[1] : domino[1] * 10 + domino[0];
         pairCount += arr[num];
         arr[num]++;
     }
