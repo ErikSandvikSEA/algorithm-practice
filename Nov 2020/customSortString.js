8cb31fef06984999cba341bf89f69670eb7aeec3
eriksandviksea <eriksandvikSEA@Gmail.com>
2020-11-11 13:16:59 -0800

    customSortString

---
+++ Nov 2020/customSortString.js
@@ -0,0 +1,28 @@
+var customSortString = function(S, T) {
+	let catalog = {};
+	let result = '';
+
+	for (let i = 0; i < S.length; i++) {
+        const char = S[i]
+		catalog[char] = 0;
+	}
+
+	for (let i = 0; i < T.length; i++) {
+        const char = T[i]
+		if (char in catalog) {
+			catalog[char]++;
+		} else {
+			result += char;
+		}
+	}
+
+	for(const char in catalog) {
+		if (catalog[char] !== 0) {
+			for (let i = 0; i < catalog[char]; i++) {
+				result += char;
+			}
+		}
+	}
+
+	return result;
+};
